7b1b96e14edf695989d611571ebe48c5ff713560
GDA0 <gideondelaliadeti0@gmail.com>
2024-06-22 11:35:16 +0000

    Work on header and footer contents

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 function App() {
 	return (
-		<div>
-			<header></header>
-			<main></main>
-			<footer></footer>
+		<div className="bg-light d-flex flex-column min-vh-100">
+			<header>
+				<nav className="navbar bg-body-tertiary border-bottom border-2">
+					<div className="container-fluid">
+						<span className="navbar-brand mb-0 h1 fw-bold fs-3 mx-auto">
+							Calculator
+						</span>
+					</div>
+				</nav>
+			</header>
+			<main className="flex-grow-1"></main>
+			<footer className="container text-center fw-medium text-secondary mt-auto">
+				<p>
+					&copy; {new Date().getFullYear()} Calculator.
+					Created By{" "}
+					<a
+						className="text-primary"
+						href="https://github.com/GDA0"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Gideon D. Adeti
+					</a>
+					. All Rights Reserved.
+				</p>
+			</footer>
 		</div>
 	);
 }
